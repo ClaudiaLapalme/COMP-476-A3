efa57f0be4a088a2cb26087d37b7f454591005d9
Claudia <claudia.lapalme@gmail.com>
2021-03-30 09:58:44 -0400

    Added the balls on the path

---
+++ Assets/Scripts/Graphs/Graph.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// This script just contains the graph data structure. I have separated it from the other two files to reduce 
+// dependencies.
+namespace Graphs
+{
+    public class Graph
+    {
+        #region public variables
+
+        public List<Node> Nodes { get; }
+        public List<Edge> Edges { get; }
+
+        #endregion
+
+        public Graph()
+        {
+            Nodes = new List<Node>();
+            Edges = new List<Edge>();
+        }
+
+        public Graph(List<Node> nodes, List<Edge> edges)
+        {
+            Nodes = nodes;
+            Edges = edges;
+        }
+    
+        public class Node
+        {
+            public Color NodeColor { get; set; }
+            public Vector3 Position { get; }
+
+            public Node() { }
+            
+            public Node(Vector3 position)
+            {
+                NodeColor = Color.red;
+                Position = position;
+            }
+            
+            public Node(Vector3 position, Color colour)
+            {
+                NodeColor = colour;
+                Position = position;
+            }
+        }
+
+        public class Edge
+        {
+            public Color EdgeColor { get; }
+            public Node Node1 { get; }
+            public Node Node2 { get; }
+        
+            public Edge(Node node1, Node node2)
+            {
+                EdgeColor = Color.cyan;
+                Node1 = node1;
+                Node2 = node2;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Graphs/TileGraph.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+// In this script I populate the tile graph with equidistant nodes and edges.
+// If an edge or node is too close to or collides with an object, it is not added to the graph.
+namespace Graphs
+{
+    public class TileGraph : MonoBehaviour
+    {
+        #region private variables
+
+        private const int MAXWidth = 31;
+        private const int MAXLength = 28;
+
+        private const float IniXPos = 8.5f; // going towards 0 = going to the right
+        private const float IniYPos = 22.2f;
+        private const float IniZPos = -2.6f; // going towards 0 = going down
+        private const float CollisionSphereRadius = 0.4f;
+        
+        #endregion
+        
+        public static Graph Graph;
+
+        public static void Initialize()
+        {
+            Graph = new Graph();
+            AddNodes();
+            AddEdges();
+        }
+
+        private static void AddNodes()
+        {
+            const int layerMask = 1 << 0; // only check for collisions on the default layer
+            var currentPos = new Vector3(IniXPos, IniYPos, IniZPos);
+
+            for (var i = 0; i < MAXLength; i++) // up to down
+            {
+                for (var j = 0; j < MAXWidth; j++) // left to right
+                {
+                    // if there are no objects in this space, add the node to the graph
+                    if (!Physics.CheckSphere(currentPos, CollisionSphereRadius, layerMask))
+                    {
+                        Graph.Nodes.Add(new Graph.Node(currentPos));
+                    }
+
+                    currentPos.x++;
+                }
+
+                currentPos.x = IniXPos;
+                currentPos.z++;
+            }
+        }
+
+        private static void AddEdges()
+        {
+            for (var i = 0; i < Graph.Nodes.Count - 1; i++)
+            {
+                var nodePos = Graph.Nodes[i].Position;
+
+                var nodeRight =
+                    Graph.Nodes.Find(
+                        node => node.Position == new Vector3(nodePos.x + 1, nodePos.y, nodePos.z));
+                var nodeTopRight =
+                    Graph.Nodes.Find(node =>
+                        node.Position == new Vector3(nodePos.x + 1, nodePos.y, nodePos.z + 1));
+                var nodeBottomRight =
+                    Graph.Nodes.Find(node =>
+                        node.Position == new Vector3(nodePos.x + 1, nodePos.y, nodePos.z - 1));
+                var nodeBottom =
+                    Graph.Nodes.Find(
+                        node => node.Position == new Vector3(nodePos.x, nodePos.y, nodePos.z - 1));
+
+                if (nodeRight != null && !Physics.Linecast(nodePos, nodeRight.Position))
+                {
+                    Graph.Edges.Add(new Graph.Edge(Graph.Nodes[i], nodeRight));
+                }
+
+                if (nodeTopRight != null && !Physics.Linecast(nodePos, nodeTopRight.Position))
+                {
+                    Graph.Edges.Add(new Graph.Edge(Graph.Nodes[i], nodeTopRight));
+                }
+
+                if (nodeBottomRight != null && !Physics.Linecast(nodePos, nodeBottomRight.Position))
+                {
+                    Graph.Edges.Add(new Graph.Edge(Graph.Nodes[i], nodeBottomRight));
+                }
+
+                if (nodeBottom != null && !Physics.Linecast(nodePos, nodeBottom.Position))
+                {
+                    Graph.Edges.Add(new Graph.Edge(Graph.Nodes[i], nodeBottom));
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Graph == null)
+            {
+                Initialize();
+            }
+            else
+            {
+                foreach (var node in Graph.Nodes)
+                {
+                    Gizmos.color = node.NodeColor;
+                    Gizmos.DrawSphere(node.Position, 0.30f);
+                }
+            }
+        }
+    }
+}
