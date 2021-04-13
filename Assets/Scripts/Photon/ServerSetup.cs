0cbc7eae3c84606d73ccd11024ba12c57d5ec483
Claudia <claudia.lapalme@gmail.com>
2021-04-13 15:23:06 -0400

    Hooked up the network between two games

---
+++ Assets/Scripts/Photon/ServerSetup.cs
@@ -0,0 +1,59 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Photon
+{
+    public class ServerSetup : MonoBehaviourPunCallbacks
+    {
+        public void Connect()
+        {
+            Debug.Log("--> <color=green>CONNECTED to the name server</color> <--");
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public void CreateRoom()
+        {
+            Debug.Log("--> <color=yellow>Attempting to JOIN a room</color> <--");
+            PhotonNetwork.CreateRoom("Roomba");
+        }
+
+        public void JoinRandomRoom()
+        {
+            Debug.Log("--> <color=yellow>Attempting to CREATE a room</color> <--");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnConnected()
+        {
+            Debug.Log("--> <color=green>CONNECTED to the name server</color> <--");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("<color=green>CONNECTED TO MASTER</color>");
+            JoinRandomRoom();
+        }
+
+        public override void OnCreatedRoom()
+        {
+            Debug.Log("--> <color=green>Successfully CREATED a room</color> <--");
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("--> <color=green>Successfully JOINED a room</color> <--");
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log($"Return code: {returnCode}, message: {message}");
+            Debug.Log("--> <color=orange>Could NOT join a random room. Attempting to create a room instead</color> <--");
+            CreateRoom();
+        }
+
+        private void Start()
+        {
+            Connect();
+        }
+    }
+}
